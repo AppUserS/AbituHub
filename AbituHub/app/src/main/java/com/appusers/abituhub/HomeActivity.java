e411978bcdb9d4a610ec933185c62ce9bc0c5f77
devBeginer <sfr007@ya.ru>
2016-08-19 18:53:09 +0300

    new changes

--- AbituHub/app/src/main/java/com/appusers/abituhub/HomeActivity.java
+++ AbituHub/app/src/main/java/com/appusers/abituhub/HomeActivity.java
@@ -1,4 +1,28 @@
 package com.appusers.abituhub;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity{
+    Button button;
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+        Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id. button2);
+    }
+
+    public void setButtonClick(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void setButton2Click(View view){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
 }
--- AbituHub/app/src/main/java/com/appusers/abituhub/StartActivity.java
+++ AbituHub/app/src/main/java/com/appusers/abituhub/StartActivity.java
@@ -1,13 +1,23 @@
 package com.appusers.abituhub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(StartActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5*1000);
     }
 }
